3f91ebcd6deafb6630387183126009cc2af01b6e
croeer <christian@roeer.info>
2023-02-11 20:23:53 +0100

    fix footer zorder error (react mui sticky footer template)

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -1,18 +1,51 @@
-import React, { FC, ReactElement } from "react";
-import { Paper, Stack } from "@mui/material";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { Stack } from "@mui/material";
 import ApiHealthCheck from "./ApiHealthCheck";
 
-export const Footer: FC = (): ReactElement => {
+function Copyright() {
   return (
-    <Paper
-      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
-      elevation={0}
-    >
-      <Stack direction="row" justifyContent="right">
-        <ApiHealthCheck />
-      </Stack>
-    </Paper>
+    <Typography variant="body2" color="text.secondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
   );
-};
+}
 
-export default Footer;
+export default function StickyFooter() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "20vh",
+      }}
+    >
+      <CssBaseline />
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: "auto",
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[200]
+              : theme.palette.grey[800],
+        }}
+      >
+        <Stack direction="row" justifyContent="right">
+          <ApiHealthCheck />
+        </Stack>
+      </Box>
+    </Box>
+  );
+}
